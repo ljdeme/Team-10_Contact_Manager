f01759b91970a1de517222823070a95f8b82626c
Bhumika Sood <bhumikajoytisood@gmail.com>
2023-01-26 14:20:29 -0500

    Uploading API files to repository
     Changes to be committed:
    	new file:   LAMPAPI/AddColor.php
    	new file:   LAMPAPI/AddContact.php
    	new file:   LAMPAPI/DeleteContact.php
    	new file:   LAMPAPI/Login.php
    	new file:   LAMPAPI/Register.php
    	new file:   LAMPAPI/SearchColors.php
    	new file:   LAMPAPI/SearchContacts.php
    	new file:   LAMPAPI/UpdateContact.php

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,40 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$FirstName = $inData["FirstName"];
+    $LastName = $inData["LastName"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? and LastName = ? and userID = ?");
+		$stmt->bind_param("sss", $FirstName, $LastName, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
