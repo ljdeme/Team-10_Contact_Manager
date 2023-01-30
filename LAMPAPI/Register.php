d5c95c69a8ce93a46f0cb61ddbf58c66b945ecab
ljdeme <julia_demetita@knights.ucf.edu>
2023-01-30 16:53:13 -0500

    organizing repository

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -15,35 +15,25 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT Login FROM Users WHERE Login = ?");
-		$stmt->bind_param("s", $Login);
+		/*$stmt = $conn->prepare("SELECT Login FROM Users WHERE Login = ?");
+		$stmt->bind_param("s", $login);
 		$stmt->execute();
-		$duplicateLogin = $stmt->get_result();
-		$stmt->close();
-	
-		$stmt = $conn->prepare("SELECT Email FROM Users WHERE Email = ?");
-		$stmt->bind_param("s", $Email);
-		$stmt->execute();
-		$duplicateEmail = $stmt->get_result();
+		$result = $stmt->get_result();
 		$stmt->close();
 
-		if (($duplicateLogin->num_rows == 0) && ($duplicateEmail->num_rows == 0))
-		{
+		if (is_null($result))
+		{*/
 			$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Phone, Email, Login, Password) VALUES (?,?,?,?,?,?)");
 			$stmt->bind_param("ssssss", $FirstName, $LastName, $Phone, $Email, $Login, $Password);
 			$stmt->execute();
 			$stmt->close();
 			$conn->close();
 			returnWithError("");
-		}
-		else if (($duplicateLogin->num_rows > 0) && ($duplicateEmail->num_rows == 0))
+		//}
+		/*(else
 		{
 			returnWithError("Username already taken");
-		}
-		else
-		{
-			returnWithError("User with email already exists");
-		}
+		}*/
 	}
 
    	function getRequestInfo()
