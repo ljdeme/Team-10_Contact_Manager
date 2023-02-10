3951feff1e698ba0cfec1a21837abb2070c79f69
Nicolas Gonzalez <77816986+Nick1052@users.noreply.github.com>
2023-02-10 04:20:20 -0500

    Update SearchContacts.php
    Changed the output format of the SearchAPI

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -13,9 +13,9 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("SELECT Name, Phone, Email, ContactID FROM Contacts WHERE UserID = ? AND (Name LIKE ? OR Phone LIKE ? OR Email LIKE ?)");
+		$stmt = $conn->prepare("SELECT FirstName, LastName, Phone, Email, ContactID FROM Contacts WHERE UserID = ? AND (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?)");
 		$Search = "%". $inData["Search"] . "%";
-		$stmt->bind_param("ssss", $inData["UserID"], $Search, $Search, $Search);
+		$stmt->bind_param("sssss", $inData["UserID"], $Search, $Search, $Search, $Search);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
@@ -27,7 +27,14 @@
 				$searchResults .= ",";
 			}
 			$searchCount++;
-			$searchResults .= '"'. $row["Name"] ." ". $row["Phone"] ." ". $row["Email"] .'"';
+			$searchResults .= 
+			'{'
+				.'"FirstName":"'. $row["FirstName"] .'",'.
+				'"LastName":"'. $row["LastName"] .'",'.
+				'"Phone":"'. $row["Phone"] .'",'.
+				'"Email":"'. $row["Email"] .'",'.
+				'"ContactID":"'. $row["ContactID"].'"'.
+			'}';
 		}
 		
 		if( $searchCount == 0 )
